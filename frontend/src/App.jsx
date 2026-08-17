7cc18236a8fdd780601f651f22f0e685a5962eab
sssuryaananth <256953659+sssuryaananth@users.noreply.github.com>
2026-08-18 03:18:52 +0530

    add backend dependencies for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,7 +63,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path = "/login" element={<Login/>}/>
+      <Route path = "/Login" element={<Login/>}/>
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
